0c3bcaed5580f09b17c33cfe5e37b8ac58f8a305
nephros <nemo@pgxperia10>
2020-03-20 11:16:25 +0100

    improve test file

--- test.sh
+++ test.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 if [[ ! -d $1 ]] ; then
-echo $1 not a dir, must point to a dir with images, exit.
+echo "$1 not a dir, must point to a dir (e.g. ../../rpmbuild/BUILD/ImageMagick-7.0.10-1/images), exit."
 exit 1
 fi
-tmp=$( mktemp -d )
-mkdir -p $tmp
-for inp in $1/tests/*miff
+tmp=$( mktemp -d /tmp/imtest_XXXXX)
+revfmt=png
+
+for inp in $1/images/*
   do
-	for fmt in pdf bmp png gif tiff jpg 
+	[[ -f $inp ]] || continue
+	echo ${inp##*/}
+	for fmt in pdf bmp png gif tiff jpg xbm xpm tga ps pnm pgx
   	do
-		convert $inp $tmp/out.${fmt} && echo "${inp##*/} to $fmt ok."
-		convert $tmp/out.${fmt} $tmp/re${fmt}.png && echo "$fmt reverse ok." 
+		convert $inp -resize x240 $tmp/out.${fmt} || echo "${inp##*/} to $fmt failed."
+		#convert $tmp/out.${fmt} $tmp/reverse${fmt}.$revfmt || echo "$fmt reverse to $revfmt failed." 
   	done
-		convert $inp -resize x240 $tmp/out.jpg && echo "${inp##*/} resize ok."
+		#convert $inp -resize x240 $tmp/${inp##*/}resized_out.jpg || echo "${inp##*/} resize failed."
   done
+echo conversion runs done. Enter to continue.
+read dummy
 file $tmp/*
+echo \'file\' results shown above. Enter to continue.
+read dummy
+echo =================================
 convert -version
+echo =================================
 rm -r $tmp
+echo cleaning up
+echo all done.
