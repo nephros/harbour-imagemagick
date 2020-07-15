db7ec4381a9e92e32e58f15e2ab0ec851cfe0c13
nephros <nemo@pgxperia10>
2020-07-15 10:51:06 +0200

    allow non-installed convert binary.

--- test.sh
+++ test.sh
@@ -6,15 +6,22 @@ fi
 tmp=$(mktemp -d -t imtest_XXXXXX)
 revfmt=png
 
+if [[ "x$CONVERT" = "x" ]]; then
+	cvt=convert
+else
+	echo using custom convert binary
+	cvt=$CONVERT
+fi
+
 for inp in $1/*
   do
 	[[ -f $inp ]] || continue
 	echo ${inp##*/}
 	for fmt in pdf bmp png gif tiff jpg xbm xpm tga ps pnm pgx
-  	do
-		convert $inp -resize x240 $tmp/out.${fmt} || echo "${inp##*/} to $fmt failed."
+	do
+		$cvt $inp -resize x240 $tmp/out.${fmt} || echo "${inp##*/} to $fmt failed."
 		#convert $tmp/out.${fmt} $tmp/reverse${fmt}.$revfmt || echo "$fmt reverse to $revfmt failed." 
-  	done
+	done
 		#convert $inp -resize x240 $tmp/${inp##*/}resized_out.jpg || echo "${inp##*/} resize failed."
   done
 echo conversion runs done. Enter to continue.
